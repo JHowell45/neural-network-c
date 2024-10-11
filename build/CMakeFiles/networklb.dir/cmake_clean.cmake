9df0b2854a88f30d534f86209133c6f756e4ded4
= <jhowell45@googlemail.com>
2024-10-11 19:40:56 +0100

    Basic cmake file and dir structure

---
+++ build/CMakeFiles/networklb.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/networklb.dir/network/lib/src/vector.c.o"
+  "CMakeFiles/networklb.dir/network/lib/src/vector.c.o.d"
+  "libnetworklb.a"
+  "libnetworklb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/networklb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
