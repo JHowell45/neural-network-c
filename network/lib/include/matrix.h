edbd19a9ada90f3848542bedfc937b17fc921908
= <jhowell45@googlemail.com>
2024-10-11 22:08:45 +0100

    basic header file for the matrix impl.

--- network/lib/include/matrix.h
+++ network/lib/include/matrix.h
@@ -1,8 +1,19 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-typedef struct {
+#include <stdlib.h>
+#include <vector.h>
 
+typedef struct {
+  size_t rows;
+  size_t columns;
+  vector_t *items;
 } matrix_t;
+
+matrix_t *new_matrix(size_t rows, size_t cols);
+void free_matrix(matrix_t *m);
+void display_matrix(matrix_t *m);
+
+void matrix_insert(matrix_t *m, size_t x, size_t y, double value);
 
 #endif
