40c5439aa1c59bd4463b88f0dfe86bb8c51df354
= <jhowell45@googlemail.com>
2024-10-11 22:00:57 +0100

    basic vector impl.

--- network/lib/src/vector.c
+++ network/lib/src/vector.c
@@ -1,10 +1,11 @@
 #include "vector.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 vector_t *new_vector(size_t capacity) {
   vector_t *vec = malloc(sizeof(vector_t));
   vec->capacity = capacity;
-  vec->size = 0;
+  vec->size = capacity;
   vec->items = calloc(vec->capacity, sizeof(double));
   return vec;
 }
@@ -14,6 +15,16 @@ void free_vector(vector_t *vec) {
   }
   free(vec);
 }
-void display_vector(vector_t *vec) {}
+void display_vector(vector_t *vec) {
+  printf("[");
+  for (register int i = 0; i < vec->size; i++) {
+    if (i == 0) {
+      printf("%f", vec->items[i]);
+    } else {
+      printf(", %f", vec->items[i]);
+    }
+  }
+  printf("]");
+}
 
 void vector_insert(vector_t *vec, size_t index, double value) {}
--- network/main.c
+++ network/main.c
@@ -1,7 +1,10 @@
 #include "vector.h"
+#include <stdio.h>
 
 int main(void) {
   vector_t *vec = new_vector(10);
+  display_vector(vec);
+  printf("\n");
   free_vector(vec);
   return 0;
 }
