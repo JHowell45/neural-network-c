8ceb0bb37e1abc50c277462ce92235568548f1ca
= <jhowell45@googlemail.com>
2024-10-14 21:48:56 +0100

    test inserts for matrices.

--- tests/matrices/matrix_insert.c
+++ tests/matrices/matrix_insert.c
@@ -18,17 +18,19 @@ static void test_initial_value_is_zero_for_created_matrix(void) {
   free_matrix(m);
 }
 
-// static void test_insert_value_to_matrix(void) {
-//   double val = 5.0;
-//   matrix_t *vec = new_matrix(3);
-//   matrix_insert(vec, 0, val);
-//   TEST_ASSERT_EQUAL_INT(matrix_get(vec, 0), val);
-//   free_matrix(vec);
-// }
+static void test_insert_value_to_matrix(void) {
+  size_t row_i = 0;
+  size_t col_i = 0;
+  double val = 5.0;
+  matrix_t *m = new_matrix(MATRIX_ROWS, MATRIX_COLS);
+  matrix_insert(m, row_i, col_i, val);
+  TEST_ASSERT_EQUAL_DOUBLE(matrix_get(m, row_i, col_i), val);
+  free_matrix(m);
+}
 
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_initial_value_is_zero_for_created_matrix);
-  //   RUN_TEST(test_insert_value_to_matrix);
+  RUN_TEST(test_insert_value_to_matrix);
   return UNITY_END();
 }
