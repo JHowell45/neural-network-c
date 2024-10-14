77e797db7cb4a099172b02964d58a6a5b6a4017c
= <jhowell45@googlemail.com>
2024-10-14 21:25:09 +0100

    wrote first basic test

--- tests/vectors/vector_insert.c
+++ tests/vectors/vector_insert.c
@@ -5,7 +5,13 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
+static void test_insert_value_to_vector(void) {
+  vector_t *vec = new_vector(3);
+  TEST_ASSERT_EQUAL_INT(vector_get(vec, 0), 0);
+}
+
 int main(void) {
   UNITY_BEGIN();
+  RUN_TEST(test_insert_value_to_vector);
   return UNITY_END();
 }
