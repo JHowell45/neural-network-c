65675bff06def0a99bce83a682a16f35936a8fe5
= <jhowell45@googlemail.com>
2024-10-14 20:31:03 +0100

    removed size as capacity is the same value. Also basic impl for vector multi.

--- network/lib/include/vector.h
+++ network/lib/include/vector.h
@@ -6,7 +6,6 @@
 typedef struct {
   double *items;
   size_t capacity;
-  size_t size;
 } vector_t;
 
 vector_t *new_vector(size_t capacity);
@@ -17,6 +16,6 @@ void vector_insert(vector_t *vec, size_t index, double value);
 double vector_get(vector_t *vec, size_t index);
 
 void vector_multiply_scalar(vector_t *vec, double scalar);
-void vector_multiply_vector(vector_t *vec1, vector_t *vec2);
+int vector_multiply_vector(vector_t *vec1, vector_t *vec2);
 
 #endif
--- network/lib/src/vector.c
+++ network/lib/src/vector.c
@@ -5,7 +5,6 @@
 vector_t *new_vector(size_t capacity) {
   vector_t *vec = malloc(sizeof(vector_t));
   vec->capacity = capacity;
-  vec->size = capacity;
   vec->items = calloc(vec->capacity, sizeof(double));
   return vec;
 }
@@ -17,7 +16,7 @@ void free_vector(vector_t *vec) {
 }
 void display_vector(vector_t *vec) {
   printf("[");
-  for (register int i = 0; i < vec->size; i++) {
+  for (register int i = 0; i < vec->capacity; i++) {
     if (i == 0) {
       printf("%f", vec->items[i]);
     } else {
@@ -32,3 +31,19 @@ void vector_insert(vector_t *vec, size_t index, double value) {
 }
 
 double vector_get(vector_t *vec, size_t index) { return vec->items[index]; }
+
+void vector_multiply_scalar(vector_t *vec, double scalar) {
+  for (int index = 0; index < vec->capacity; index++) {
+    vec->items[index] = vec->items[index] * scalar;
+  }
+}
+double *vector_multiply_vector(vector_t *vec1, vector_t *vec2) {
+  if (vec1->capacity != vec2->capacity) {
+    return NULL;
+  }
+  double sum = 0.0;
+  for (int i = 0; i < vec1->capacity; i++) {
+    sum += vec1->items[i] * vec2->items[i];
+  }
+  return &sum;
+}
