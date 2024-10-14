f67b2033fb13b6256f94a7053225907c1a7d68b9
= <jhowell45@googlemail.com>
2024-10-14 22:34:30 +0100

    creating weights struct to manage growing the array of weights

--- network/lib/include/nodes.h
+++ network/lib/include/nodes.h
@@ -5,24 +5,36 @@
 
 typedef struct node node_t;
 typedef struct weight weight_t;
-
-struct node {
-  double value;
-  double bias;
-  weight_t *weights;
-};
+typedef struct weights weights_t;
 
 struct weight {
   double value;
   node_t *node;
 };
 
+struct weights {
+  size_t capacity;
+  size_t size;
+  weight_t *items;
+};
+
+struct node {
+  double value;
+  double bias;
+  weights_t *weights;
+};
+
+weight_t *new_weight(double *value, node_t *n);
+void free_weight(weight_t *weight);
+
+weights_t *new_weights(size_t capacity);
+void free_weights(weights_t *w);
+
+void weights_add_weight(weights_t *w, weight_t *item);
+
 node_t *new_node(double value, double bias);
 void free_node(node_t *node);
 
 void node_add_weight(node_t *node, weight_t *weight);
-
-weight_t *new_weight(double *value, node_t *n);
-void free_weight(weight_t *weight);
 
 #endif
