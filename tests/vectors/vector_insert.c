e6aa684036fdcdfeb2b70154fe1d5887045988f6
= <jhowell45@googlemail.com>
2024-10-14 21:26:33 +0100

    basic test for inserting a value for the vector.

--- tests/vectors/vector_insert.c
+++ tests/vectors/vector_insert.c
@@ -5,9 +5,18 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
-static void test_insert_value_to_vector(void) {
+static void test_initial_value_is_zero_for_created_vector(void) {
   vector_t *vec = new_vector(3);
   TEST_ASSERT_EQUAL_INT(vector_get(vec, 0), 0);
+  free_vector(vec);
+}
+
+static void test_insert_value_to_vector(void) {
+  double val = 5.0;
+  vector_t *vec = new_vector(3);
+  vector_insert(vec, 0, val);
+  TEST_ASSERT_EQUAL_INT(vector_get(vec, 0), val);
+  free_vector(vec);
 }
 
 int main(void) {
