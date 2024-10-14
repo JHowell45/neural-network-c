492cc5fd4fd17fec90b731ae22978688fd1b9906
= <jhowell45@googlemail.com>
2024-10-14 22:28:37 +0100

    redoing the nodes

--- network/lib/include/nodes.h
+++ network/lib/include/nodes.h
@@ -3,15 +3,6 @@
 
 #include "vector.h"
 
-typedef struct {
-  double value;
-  double weight;
-} input_node_t;
-
-typedef struct {
-  double bias;
-} layer_node_t;
-
 typedef struct node node_t;
 typedef struct weight weight_t;
 
@@ -25,13 +16,5 @@ struct weight {
   double value;
   node_t *node;
 };
-
-input_node_t *new_input_node(double value, double weight);
-void free_input_node(input_node_t *n);
-
-layer_node_t *new_layer_node(double weight, double bias);
-void free_layer_node(layer_node_t *n);
-
-double activation_function(layer_node_t *lnode, vector_t *inputs);
 
 #endif
--- network/lib/src/nodes.c
+++ network/lib/src/nodes.c
@@ -1,23 +1,2 @@
 #include "nodes.h"
 #include <stdlib.h>
-
-input_node_t *new_input_node(double value, double weight) {
-  input_node_t *n = malloc(sizeof(input_node_t));
-  n->value = value;
-  n->weight = weight;
-  return n;
-}
-
-void free_input_node(input_node_t *n) { free(n); }
-
-layer_node_t *new_layer_node(double weight, double bias) {
-  layer_node_t *n = malloc(sizeof(layer_node_t));
-  n->bias = bias;
-  return n;
-}
-
-void free_layer_node(layer_node_t *n) { free(n); }
-
-double activation_function(layer_node_t *lnode, vector_t *inputs) {
-  return lnode->bias + vector_sum(inputs);
-}
