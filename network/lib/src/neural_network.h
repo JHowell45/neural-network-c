c43b08e871a84ff7cf1bef346a0fd491337947d0
= <jhowell45@googlemail.com>
2024-10-17 00:10:20 +0100

    adding clang formatting

---
+++ network/lib/src/neural_network.h
@@ -0,0 +1,16 @@
+#include "neural_network.h"
+
+neural_network_t* new_neural_network(size_t inputs_size, size_t outputs_size,
+                                     activation_function_t activation_type)
+{
+}
+void free_neural_network(neural_network_t* network) {}
+
+vector_t* neural_network_inference(neural_network_t* network, vector_t* inputs)
+{
+}
+
+void neural_network_add_hidden_layer(neural_network_t* network,
+                                     size_t neuron_count)
+{
+}
--- network/main.c
+++ network/main.c
@@ -1,57 +1,64 @@
+#include <stdio.h>
+
 #include "matrix.h"
 #include "vector.h"
-#include <stdio.h>
 
 #define VECTOR_SIZE 10
 
 #define MATRIX_WIDTH 3
 #define MATRIX_HEIGHT 4
 
-void vector_test() {
-  vector_t *vec = new_vector(VECTOR_SIZE);
-  for (int i = 0; i < VECTOR_SIZE; i++) {
-    vector_insert(vec, i, i + 1);
-  }
-  display_vector(vec);
-  printf("\n");
+void vector_test()
+{
+    vector_t* vec = new_vector(VECTOR_SIZE);
+    for (int i = 0; i < VECTOR_SIZE; i++)
+    {
+        vector_insert(vec, i, i + 1);
+    }
+    display_vector(vec);
+    printf("\n");
 
-  vector_t *vec2 = vector_multiply_scalar(vec, 5);
-  display_vector(vec2);
-  printf("\n");
+    vector_t* vec2 = vector_multiply_scalar(vec, 5);
+    display_vector(vec2);
+    printf("\n");
 
-  double value = vector_multiply_vector(vec, vec2);
-  printf("Multiply both vectors above: %f\n", value);
+    double value = vector_multiply_vector(vec, vec2);
+    printf("Multiply both vectors above: %f\n", value);
 
-  free_vector(vec);
-  free_vector(vec2);
+    free_vector(vec);
+    free_vector(vec2);
 }
 
-void matrix_test() {
-  matrix_t *m = new_matrix(MATRIX_WIDTH, MATRIX_HEIGHT);
-  display_matrix(m);
-  int value = 1;
-  printf("MATRIX_WIDTH: %d\n", MATRIX_WIDTH);
-  printf("MATRIX_HEIGHT: %d\n", MATRIX_HEIGHT);
-  for (int column = 0; column < MATRIX_WIDTH; column++) {
-    for (int row = 0; row < MATRIX_HEIGHT; row++) {
-      matrix_insert(m, column, row, value);
-      value++;
+void matrix_test()
+{
+    matrix_t* m = new_matrix(MATRIX_WIDTH, MATRIX_HEIGHT);
+    display_matrix(m);
+    int value = 1;
+    printf("MATRIX_WIDTH: %d\n", MATRIX_WIDTH);
+    printf("MATRIX_HEIGHT: %d\n", MATRIX_HEIGHT);
+    for (int column = 0; column < MATRIX_WIDTH; column++)
+    {
+        for (int row = 0; row < MATRIX_HEIGHT; row++)
+        {
+            matrix_insert(m, column, row, value);
+            value++;
+        }
     }
-  }
-  display_matrix(m);
+    display_matrix(m);
 
-  matrix_t *tM = matrix_transpose(m);
-  display_matrix(tM);
+    matrix_t* tM = matrix_transpose(m);
+    display_matrix(tM);
 
-  free_matrix(m);
-  free_matrix(tM);
+    free_matrix(m);
+    free_matrix(tM);
 }
 
 void nodes_test() {}
 
-int main(void) {
-  //   vector_test();
-  //   matrix_test();
-  nodes_test();
-  return 0;
+int main(void)
+{
+    //   vector_test();
+    //   matrix_test();
+    nodes_test();
+    return 0;
 }
