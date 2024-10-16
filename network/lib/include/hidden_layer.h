58289f9a5c69a68868f23db43ba7e367bcd88a67
= <jhowell45@googlemail.com>
2024-10-16 23:29:40 +0100

    basic headers and methods

--- network/lib/include/hidden_layer.h
+++ network/lib/include/hidden_layer.h
@@ -1,18 +1,26 @@
 #ifndef HIDDEN_LAYER_H
 #define HIDDEN_LAYER_H
 
+#include "weights.h"
+#include <stdlib.h>
+
+enum AcitvationFunction { Linear, Sigmoid, TanH, ReLU };
 typedef struct {
+  double value;
   double bias;
 } neuron_t;
 
 neuron_t *new_neuron(double bias);
 void free_neuron(neuron_t *n);
 
+double neuron_activation_function(neuron_t *neuron, weights_t **weights,
+                                  AcitvationFunction function_type);
+
 typedef struct {
   neuron_t **neurons;
 } hidden_layer_t;
 
-hidden_layer_t *new_hidden_layer();
+hidden_layer_t *new_hidden_layer(size_t number_of_neurons);
 void free_hidden_layer(hidden_layer_t *layer);
 
 #endif
