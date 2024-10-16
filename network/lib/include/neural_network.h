447bbc040bfd1547474b0b7ccfa21ac240963862
= <jhowell45@googlemail.com>
2024-10-17 00:03:01 +0100

    method for adding a hidden layer to the neural network.

--- network/lib/include/neural_network.h
+++ network/lib/include/neural_network.h
@@ -21,4 +21,7 @@ void free_neural_network(neural_network_t *network);
 
 vector_t *neural_network_inference(neural_network_t *network, vector_t *inputs);
 
+void neural_network_add_hidden_layer(neural_network_t *network,
+                                     size_t neuron_count);
+
 #endif
